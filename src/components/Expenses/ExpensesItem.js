ac120c3026e40cd8fc8ca19f3f6c885fd4fb0904
Zeyaul Mustfa <zeyaul@opendatafabric.com>
2022-03-20 02:21:49 +0530

    first push

---
+++ src/components/Expenses/ExpensesItem.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./ExpensesItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHendler = () =>{
+    setTitle('Updated!!');
+    console.log(title);
+  };
+
+  return (
+
+    <Card className="expense-item">
+        <ExpenseDate date={props.date}/>
+      
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+      <button onClick={clickHendler}> Change Title</button>
+    </Card>
+  );
+}
+export default ExpenseItem;
